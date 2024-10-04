f739c571ac98735522466f432c1b5918d65df0c2
JorgeCM <jorgecarma@gmail.com>
2024-10-04 23:47:02 +0200

    ej8

--- Python/prework/ej8.py
+++ Python/prework/ej8.py
@@ -1,2 +1,8 @@
 #Ejercicio 8 -> Cálculo del indice de masa corporal (IMC)
 peso = float(input("Introduce el peso en kilogramos: "))
+
+altura = float(input("Introduce tu altura: "))
+
+imc = peso / (altura ** 2)
+
+print(f"Tu indice de masa es {imc}")
